d7034558f8a6bb534231e88a2bfd4fb0d590e5b9
david <dhernandez@intersysconsulting.com>
2019-08-15 15:23:00 -0500

    Adding composite design pattern

---
+++ src/com/david/designpatterns/composite/Company.java
@@ -0,0 +1,35 @@
+package com.david.designpatterns.composite;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Company {
+
+  Map<String, Employee> employees = null;
+  private String name;
+
+  public Company(String name) {
+    this.name = name;
+    employees = new HashMap<>();
+  }
+
+  public void addEmployee(String position, Employee employee) {
+    employees.put(position, employee);
+  }
+
+  public void removeEmployee(String employee) {
+    employees.remove(employee);
+  }
+
+  public void showDetails() {
+    System.out.println("Details about this company");
+    System.out.println("Company name = " + name);
+    System.out.println("########################");
+    System.out.println("List of employees\n");
+    employees.forEach(
+        (key, value) -> {
+          System.out.println("Position: " + key.toUpperCase());
+          value.showDetails();
+        });
+  }
+}
