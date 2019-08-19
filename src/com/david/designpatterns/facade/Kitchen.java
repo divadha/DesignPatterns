1835d9a1cc929b3b3c0bd3066e1db2ecbcd5b294
david <dhernandez@intersysconsulting.com>
2019-08-19 11:33:48 -0500

    Adding Facade design pattern

---
+++ src/com/david/designpatterns/facade/Kitchen.java
@@ -0,0 +1,11 @@
+package com.david.designpatterns.facade;
+
+public class Kitchen {
+
+  public boolean cockingFood(String food) {
+    Chef chef = new Chef("Mario");
+    System.out.println("\t\t\tchef " + chef.getName() + " is preparing " + food);
+
+    return true;
+  }
+}
