ac8deda279deb70d0dcaa9162693921956a3988a
David <dhernandez@intersysconsulting.com>
2019-09-23 10:37:39 -0500

    Adding Facade design pattern

--- src/com/david/designpatterns/facade/TestFacade.java
+++ src/com/david/designpatterns/facade/TestFacade.java
@@ -3,8 +3,8 @@ package com.david.designpatterns.facade;
 public class TestFacade {
 
   public static void main(String[] args) {
-    System.out.println("***Client is going to order food in a restaurant***");
 
+    System.out.println("***Client is going to order food in a restaurant***");
     /**
      * In this case, the restaurant is the facade because we can't see what is behind the food's
      * preparation
