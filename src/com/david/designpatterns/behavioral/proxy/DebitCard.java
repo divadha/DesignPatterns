fb196094648845939a593c13b6d27db83accd4ac
David Hernandez <dhernandez@intersysconsulting.com>
2019-10-10 11:15:57 -0500

    Adding proxy design pattern

---
+++ src/com/david/designpatterns/behavioral/proxy/DebitCard.java
@@ -0,0 +1,36 @@
+package com.david.designpatterns.behavioral.proxy;
+
+public class DebitCard {
+  private Account account;
+  private String cardNumber;
+
+  public DebitCard(Account account, String cardNumber) {
+    this.account = account;
+    this.cardNumber = cardNumber;
+  }
+
+  public void getMoney(double money) {
+    if (money < account.getBalance()) {
+      account.setBalance(account.getBalance() - money);
+    } else {
+      System.out.println(
+          "Sorry you don't have enough money, your balance is " + account.getBalance());
+    }
+  }
+
+  public Account getAccount() {
+    return account;
+  }
+
+  public void setAccount(Account account) {
+    this.account = account;
+  }
+
+  public String getCardNumber() {
+    return cardNumber;
+  }
+
+  public void setCardNumber(String cardNumber) {
+    this.cardNumber = cardNumber;
+  }
+}
---
+++ src/com/david/designpatterns/behavioral/proxy/TestProxy.java
@@ -0,0 +1,22 @@
+package com.david.designpatterns.behavioral.proxy;
+
+public class TestProxy {
+  public static void main(String[] args) {
+    // Creating an Account
+
+    Account account = new Account();
+    account.setId(1);
+    account.setName("David");
+    account.setBalance(1000);
+
+    System.out.println("Initial balance: " + account);
+
+    DebitCard card = new DebitCard(account, "5434 2344 0987 3453");
+    System.out.println("Getting $200");
+    card.getMoney(200);
+    System.out.println("New balance: " + account);
+
+    System.out.println("Getting $2000");
+    card.getMoney(2000);
+  }
+}
